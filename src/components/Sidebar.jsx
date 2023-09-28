23156c260278ecec90bf2415080296e3cb32220e
Hardy Sylvestre <hardsyldev@gmail.com>
2023-09-28 09:58:58 +0300

    creat all components we need

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar";
+import Search from "./Search";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="sidebar">
+      <Navbar />
+      <Search />
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
