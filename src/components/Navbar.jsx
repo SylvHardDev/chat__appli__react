b58bbee16c2df6baad4e85efb30dce6a95c1ed15
Hardy Sylvestre <hardsyldev@gmail.com>
2023-09-28 09:57:55 +0300

    src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import photos from "../img/add.png"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <span className="logo">Chat</span>
+      <div className="user">
+        <img src={photos} alt="photos" />
+        <span>Hardy</span>
+        <button>logout</button>
+      </div>
+    </div>
   )
 }
 
