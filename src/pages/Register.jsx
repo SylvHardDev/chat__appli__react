a0ef8dc3d05c39bf301ef8a89b84288ccd8c8f88
Hardy Sylvestre <hardsyldev@gmail.com>
2023-09-29 16:15:46 +0300

    creat auth context

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,9 +4,11 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, storage, db } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc, updateDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [err, setErr] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -38,14 +40,13 @@ const Register = () => {
               uid: res.user.uid,
               displayName,
               email,
-              photoURL: downloadURL
-            })
-            await setDoc(doc(db, "userChats", res.user.uid), {})
+              photoURL: downloadURL,
+            });
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate("/")
           });
         }
       );
-
- 
     } catch (err) {
       setErr(true);
     }
