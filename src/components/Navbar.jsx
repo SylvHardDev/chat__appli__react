4fd756fca9483a01e4bec78806a0ccf27abb28a0
Hardy Sylvestre <hardsyldev@gmail.com>
2023-10-02 10:12:00 +0300

    Navbar dynamic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import photos from "../img/add.png"
+import React, { useContext } from 'react'
+// import photos from "../img/add.png"
 import { signOut } from 'firebase/auth'
 import { auth } from '../firebase'
+import { AuthContext } from '../context/AuthContext'
 
-const Navbar = () => {
+const Navbar = () => { 
+  const {currentUser} = useContext(AuthContext)
+
   return (
     <div className='navbar'>
       <span className="logo">Chat</span>
       <div className="user">
-        <img src={photos} alt="photos" />
-        <span>Hardy</span>
+        <img src={currentUser.photoURL} alt="photos" />
+        <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>logout</button>
       </div>
     </div>
