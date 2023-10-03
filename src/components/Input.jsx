2aa6986ed9658737d9d5a0d908aefb7d9845accd
Hardy Sylvestre <hardsyldev@gmail.com>
2023-10-03 11:31:45 +0300

    mande anaty base le message

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,26 +1,89 @@
-import React from 'react'
-import Img from '../img/img.png'
-import Attach from '../img/attach.png'
-import Send from '../img/envoyer-le-message.png'
+import React, { useContext, useState } from "react";
+import Img from "../img/img.png";
+import Attach from "../img/attach.png";
+import Send from "../img/envoyer-le-message.png";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+import { Timestamp, arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { db, storage } from "../firebase";
+import { v4 as uuid } from "uuid";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 
 const Input = () => {
+  const [text, setText] = useState("");
+  const [img, setImg] = useState(null);
+
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const handleSend = async () => {
+    if (img) {
+      const storageRef = ref(storage, uuid());
+
+      const uploadTask = uploadBytesResumable(storageRef, img);
+
+      uploadTask.on(
+        (error) => {
+          // setErr(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateDoc(doc(db, "chats", data.chatId), {
+              messages: arrayUnion({
+                id: uuid(),
+                text,
+                senderId: currentUser.uid,
+                data: Timestamp.now(),
+                img: downloadURL,
+              }),
+            });
+          });
+        }
+      );
+    } else {
+      await updateDoc(doc(db, "chats", data.chatId), {
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          senderId: currentUser.uid,
+          data: Timestamp.now(),
+        }),
+      });
+    }
+
+    await updateDoc(doc(db, "userChats", currentUser.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text
+      }
+    })
+
+    setText("");
+    setImg(null);
+  };
   return (
-    <div className='input'>
-      <input type="text" placeholder='Type something...' />
+    <div className="input">
+      <input
+        type="text"
+        placeholder="Type something..."
+        onChange={(e) => setText(e.target.value)}
+      />
       <div className="send">
         <img src={Attach} alt="" />
-        <input type="file" style={{display:"none"}} id='file' />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="file"
+          onChange={(e) => setImg(e.target.files[0])}
+        />
         <label htmlFor="file">
           <img src={Img} alt="" />
         </label>
-        <button>
+        <button onClick={handleSend}>
           <img src={Send} alt="" />
-          
         </button>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,18 +1,23 @@
-import React from "react";
-import photos from "../img/avatar/femme.png";
+import React, { useContext } from "react";
+// import photos from "../img/avatar/femme.png";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
 const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
   console.log(message);
   return (
     <div className="message owner">
-      <div className="messageInfo">
+      {/* <div className="messageInfo">
         <img src={photos} alt="sary" />
         <span>vo zao</span>
       </div>
       <div className="messageContent">
         <p>Holla !!</p>
         <img src={photos} alt="sary" />
-      </div>
+      </div> */}
     </div>
   );
 };
