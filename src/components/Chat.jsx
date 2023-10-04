4d7075cbc69ff14333adacf522dfe28ade72b5db
Hardy Sylvestre <hardsyldev@gmail.com>
2023-10-04 17:05:06 +0300

    chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import Messages from "./Messages";
 import Input from "./Input";
 import { ChatContext } from "../context/ChatContext";
+import menuIcon from "../img/menu.png"
 
 const Chat = () => {
   const { data } = useContext(ChatContext);
@@ -9,7 +10,12 @@ const Chat = () => {
   return (
     <div className="chat">
       <div className="chatInfo">
+        <img src={data.user.photoURL} alt="" />
         <span>{data.user?.displayName}</span>
+        
+        <button className="menuIcon">
+          <img src={menuIcon} alt="" />
+        </button>
       </div>
       <Messages />
       <Input />
