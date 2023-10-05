d67420b8820349e90250d757868bb4f7d0e4aba6
Hardy Sylvestre <hardsyldev@gmail.com>
2023-10-05 10:36:07 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,15 @@ import React, { useContext } from 'react'
 import { signOut } from 'firebase/auth'
 import { auth } from '../firebase'
 import { AuthContext } from '../context/AuthContext'
+import Xmark from '../img/x.png'
 
 const Navbar = () => { 
   const {currentUser} = useContext(AuthContext)
+
+  const handleOpenMenu = () => {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.classList.toggle("visible")
+  };
 
   return (
     <div className='navbar'>
@@ -14,9 +20,9 @@ const Navbar = () => {
         <img src={currentUser.photoURL} alt="photos" />
         <span>{currentUser.displayName}</span>
       </div>
-      <button className='x'>
-        {/* <img src="" alt="" /> */}
-        X
+      <button className='x' onClick={handleOpenMenu}>
+        <img src={Xmark} alt="" />
+        {/* X */}
       </button>
       <button className='deconnexion' onClick={() => signOut(auth)}>Desconect</button>
     </div>
